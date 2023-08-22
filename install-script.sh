47a48c87af2743e5594c52823c48bbe83542b9d5
Pepijn Nichting <pepijn.nichting@gmail.com>
2023-08-22 13:06:44 +0200

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -1,83 +1,58 @@
 #!/bin/sh
 
-# Homebrew Script for OSX
-# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
+echo "Setting up your Mac..."
 
-
-if [[ $(command -v brew) == "" ]]; then
-  echo "Installing brew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Updating Homebrew"
-    brew update
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
-echo "Installing brew cask..."
-brew tap homebrew/cask
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# browsers
-echo "Installing browsers..."
-brew install --cask google-chrome
-brew install --cask firefox
-brew install --cask microsoft-edge
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
-# communication tools
-echo "Installing communication apps..."
-brew install --cask slack
-brew install --cask discord
-brew install --cask microsoft-teams
-brew install --cask microsoft-outlook
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-# basic apps
-echo "Installing basic apps..."
-brew install --cask dropbox
-brew install --cask 1password
-brew install --cask nordvpn
-brew install --cask tunnelblick
-brew install --cask the-unarchiver
-brew install --cask plex
-brew install --cask stats
-brew install --cask cakebrew
-brew install --cask copyclip
-brew install --cask spotify
-brew install --cask transmission
-brew install --cask vlc
-brew install --cask whatsapp
-brew install --cask switch
-brew install --cask steam
+# Update Homebrew recipes
+brew update
 
-# Dev Tools
-echo "Installing development tools..."
-brew install --cask ngrok
-brew install --cask phpstorm
-brew install --cask postman
-brew install --cask visual-studio-code
-brew install --cask warp
-brew install --cask docker
-brew install --cask chromedriver
-brew install --cask poedit
-brew install --cask transmit
-brew install --cask sourcetree
-brew install --cask sketch
-brew install --cask imageoptim
-brew install --cask gas-mask
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle --file $DOTFILES/Brewfile
 
+# Symlink SSH config
+if [[ ! -d $HOME/.ssh ]]
+then
+  mkdir $HOME/.ssh
+  chown 700 .ssh
+fi
 
-#  CLI tools
-echo "Installing CLI tools..."
-brew install git
-brew install node
-brew install yarn
-brew install azure-cli
-brew install azcopy
-brew install helm
-brew install mkcert
-brew install ncdu
-brew install starship
-brew install thefuck
-brew install tidy-html5
-brew install python@3.10
+if [[ ! -f $HOME/.ssh/config ]]
+then
+  ln -s $DOTFILES/settings/ssh_config $HOME/.ssh/config
+fi
 
+# Create a Projects directory
+if [[ ! -d $HOME/Projects ]]
+then
+  mkdir $HOME/Projects
+fi
 
-# manual steps
-echo "Install Bettersnaptool (https://folivora.ai/bettersnaptool/) manually from the App Store"
+# Set global gitignore file to be used
+git config --global core.excludesfile $DOTFILES/.gitignore_global
+
+# Symlink the Mackup config file to the home directory
+if [[ ! -f $HOME/.mackup.cfg ]]
+then
+  ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
+fi
+
+# Set macOS preferences - we will run this last because this will reload the shell
+# source $DOTFILES/.macos
+
